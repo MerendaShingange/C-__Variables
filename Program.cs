7829a3c2fe1e3b05bb91fc4492bf2e3f0d5bdbd0
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 11:53:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C___Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables
+            string Name = "John";
+            int age;
+            // Initiation of variables
+            age = 16;
+            Console.WriteLine("There was a boy called" + Name);
+            Console.WriteLine(Name + "was " + age + "of age");
+            Console.WriteLine(Name + "realy never liked his name");
+            
+            // changing values of variables.
+            age = 18;
+            Name = "Tom";
+            Console.WriteLine("Ar age " + age + "he changed his name to" + Name);
+
+            // to freeze the console
+            Console.ReadLine();
+
+        }
+    }
+}
